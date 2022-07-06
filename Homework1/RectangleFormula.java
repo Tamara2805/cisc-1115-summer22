943c7907abea0167c879015f0ba944f2a15a0781
tamara12 <tamaranoelgeorge@gmail.com>
2022-07-06 14:31:35 -0400

    Homework 1

---
+++ Homework1/RectangleFormula.java
@@ -0,0 +1,24 @@
+public class RectangleFormula {
+
+  //assigning variables
+  static int width = 6;
+  static int length = 7;
+
+  //equations
+  static int area = width*length;
+  static int perimeter = (2*width)+(2*length);
+
+  //ending statement
+  public static void main(String[] args) {
+
+  System.out.print("When one side is ");
+  System.out.print(width);
+  System.out.print(" and the other side is ");
+  System.out.print(length);
+  System.out.print(", the area is ");
+  System.out.print(area);
+  System.out.print(" and the perimeter is ");
+  System.out.print(perimeter);
+  System.out.println(".");
+}
+}
